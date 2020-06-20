5df4ea04f571078ac23e0d1c7f1f8c469ee56ef8
Vitalii Dolmatov <dolmatov-vs@yandex.ru>
2020-06-20 15:12:36 +0300

    Homework 2

--- src/main/java/ru/netology/domain/Product.java
+++ src/main/java/ru/netology/domain/Product.java
@@ -11,30 +11,11 @@ public class Product {
         this.price = price;
     }
 
-    public Product() {
-    }
-
     public int getId() {
         return id;
     }
 
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public int getPrice() {
-        return price;
-    }
-
-    public void setPrice(int price) {
-        this.price = price;
+    public boolean matches(String search) {
+        return name.equalsIgnoreCase(search);
     }
 }
