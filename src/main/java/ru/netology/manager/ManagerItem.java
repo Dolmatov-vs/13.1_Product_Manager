cd1a87417a44b3e63480f8c0ddc7f4e979f8de24
Vitalii Dolmatov <dolmatov-vs@yandex.ru>
2020-06-20 14:24:01 +0300

    Homework 1

--- src/main/java/ru/netology/manager/ManagerItem.java
+++ src/main/java/ru/netology/manager/ManagerItem.java
@@ -2,41 +2,23 @@ package ru.netology.manager;
 
 import ru.netology.domain.Book;
 import ru.netology.domain.Product;
+import ru.netology.domain.Smartphone;
 import ru.netology.repository.Repository;
 
 
 public class ManagerItem {
-    String text;
     Repository repository = new Repository();
 
     public ManagerItem() {
     }
 
-    public String getSearch() {
-        return text;
-    }
-
-    public void setSearch(String search) {
-        this.text = search;
-    }
-
-    public void add (Product item){
+    public void add(Product item) {
         repository.save(item);
     }
 
-    public Product[] getAll (){
-        Product[] items = repository.findAll();
-        Product[] result = new Product[items.length];
-        for (int i = 0; i < result.length; i++) {
-            int index = items.length - i - 1;
-            result[i] = items[index];
-        }
-        return result;
-    }
-
-    public Product[] searcyBy(String text) {
+    public Product[] searchBy(String text) {
         Product[] result = new Product[0];
-        for (Product product: repository.findAll()) {
+        for (Product product : repository.findAll()) {
             if (matches(product, text)) {
                 Product[] tmp = new Product[result.length + 1];
                 System.arraycopy(result, 0, tmp, 0, result.length);
@@ -46,6 +28,7 @@ public class ManagerItem {
         }
         return result;
     }
+
     public boolean matches(Product product, String search) {
         if (product instanceof Book) {
             Book book = (Book) product;
@@ -55,8 +38,16 @@ public class ManagerItem {
             if (book.getAuthor().equalsIgnoreCase(search)) {
                 return true;
             }
-            return false;
         }
-        // ваш код
+        if (product instanceof Smartphone) {
+            Smartphone smartphone = (Smartphone) product;
+            if (smartphone.getName().equalsIgnoreCase(search)) {
+                return true;
+            }
+            if (smartphone.getManufacturer().equalsIgnoreCase(search)) {
+                return true;
+            }
+        }
+        return false;
     }
 }
--- src/test/java/ru/netology/repository/RepositoryTest.java
+++ src/test/java/ru/netology/repository/RepositoryTest.java
@@ -1,5 +1,6 @@
 package ru.netology.repository;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import ru.netology.domain.Book;
 import ru.netology.domain.Product;
@@ -8,33 +9,24 @@ import ru.netology.domain.Smartphone;
 import static org.junit.jupiter.api.Assertions.*;
 
 class RepositoryTest {
-
+    Repository repository = new Repository();
     Book one = new Book(1, "10 негритят", 1000, "Агата Кристи");
     Book two = new Book(2, "Происхождение", 2000, "Дэн Браун");
     Smartphone three = new Smartphone(3, "A40", 25000, "Samsung");
     Smartphone four = new Smartphone(4, "S20", 70000, "Samsung");
-    Repository repository = new Repository();
 
-    @Test
-    void shouldAddProductAndFindAll() {
 
+    @BeforeEach
+    public void sutUp() {
         repository.save(one);
+        repository.save(two);
         repository.save(three);
-
-        Product[] expected = new Product[]{one, three};
-        Product[] actual = repository.findAll();
-
-        assertArrayEquals(expected, actual);
+        repository.save(four);
     }
 
     @Test
     void shouldRemoveById() {
         int idToRemove = 2;
-        repository.save(one);
-        repository.save(two);
-        repository.save(three);
-        repository.save(four);
-
         repository.removeById(idToRemove);
 
         Product[] expected = new Product[]{one, three, four};
