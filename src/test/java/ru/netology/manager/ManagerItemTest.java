bfb5f563148f5e40a045fa2839e43aaeb2edba2b
Vitalii Dolmatov <dolmatov-vs@yandex.ru>
2020-06-21 18:16:10 +0300

    Homework 2.2

--- src/test/java/ru/netology/manager/ManagerItemTest.java
+++ src/test/java/ru/netology/manager/ManagerItemTest.java
@@ -10,7 +10,7 @@ import ru.netology.repository.Repository;
 import static org.junit.jupiter.api.Assertions.*;
 
 class ManagerItemTest {
-    Repository repository;
+    Repository repository = new Repository();
     ManagerItem manager = new ManagerItem(repository);
     Book one = new Book(1, "10 негритят", 300, "Агата Кристи");
     Book two = new Book(2, "Происхождение", 600, "Дэн Браун");
